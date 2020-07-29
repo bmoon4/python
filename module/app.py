6f113c048eb79d50c7261e32eb486db9e93c903a
Bokyung Moon <bkmoon0702@gmail.com>
2020-07-29 15:02:38 -0400

    modules

---
+++ module/app.py
@@ -0,0 +1,5 @@
+from sales import function1, function2, Sales
+
+function1()
+function2()
+sales = Sales()
---
+++ module/sales.py
@@ -0,0 +1,11 @@
+def function1():
+    print("this is function1")
+
+
+def function2():
+    print("this is function2")
+
+
+class Sales:
+    def __init__(self):
+        print("sales obj is now constructed")
